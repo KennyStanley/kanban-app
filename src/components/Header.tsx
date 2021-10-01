e2ef009f2c198318a1204be894129c732dff8427
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-01 13:38:27 -0500

    Simplified Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,19 @@
 export type HeaderTypes = {
     title: string
     subtitle: string
-    path: string
 }
 
 const Header = (headerData: HeaderTypes) => {
     return (
         <>
-            {' '}
-            <h1 className="text-6xl font-bold">
-                {headerData.title}{' '}
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">😏</a>
-            </h1>
-            <p className="mt-6 text-2xl">
-                {headerData.subtitle}{' '}
-                <code className="bg-gray-100 p-2 ml-1 font-mono text-2xl rounded-md">
-                    {headerData.path}
-                </code>
-            </p>
+            <div className="flex items-baseline">
+                <h1 className="text-3xl font-bold">
+                    {headerData.title}
+                </h1>
+                <p className="ml-2 text-xl">
+                    {'- '}{headerData.subtitle}
+                </p>
+            </div>
         </>
     )
 }
