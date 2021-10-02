b9c24231c5b7bf08e9fb18774ce74812251d44e8
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-02 10:42:56 -0500

    Custom context for tasks

---
+++ src/contexts/TasksProvider.tsx
@@ -0,0 +1,54 @@
+import { Task } from "@/components/Task/types"
+import { createContext, useContext, useState, useCallback, Dispatch, SetStateAction } from "react";
+
+type TasksProviderOutput = {
+    tasks: Task[] | undefined
+    setTasks: Dispatch<SetStateAction<Task[]>>
+    getTaskById(id: string): Task | undefined
+    updateTask(updatedTask: Task): void
+}
+
+const TasksContext = createContext<TasksProviderOutput | undefined>(undefined)
+
+export function useTasks() {
+    return {...useContext(TasksContext)}
+}
+
+export function TasksProvider(props: {children?: any}) {
+    const [tasks, setTasks] = useState<Task[]>([
+        {
+            id: '1',
+            title: 'Make Kanban App',
+            position: 'backlog',
+            status: 'doing',
+            developer: 'Kenny'
+        },
+        {
+            id: '2',
+            title: 'Make VR Demo app',
+            details: 'Hub with links to all working VR demos',
+            position: 'backlog',
+            status: 'doing',
+            developer: 'Kenny'
+        }
+    ])
+
+    const getTaskById = useCallback((id: string) => {
+        return tasks.find(task => task.id === id)
+    }, [tasks])
+
+    const updateTask = useCallback((updatedTask: Task) => {
+        // console.log('Updating Task: ')
+        // console.log(updatedTask)
+        setTasks((prevTasks: Task[]) => prevTasks.map(task => {
+            if (task.id !== updatedTask.id) return task
+            else return updatedTask
+        }))
+    }, [setTasks])
+
+    return (
+        <TasksContext.Provider value={{tasks, setTasks, getTaskById, updateTask}}>
+            {props.children}
+        </TasksContext.Provider>
+    )
+}
