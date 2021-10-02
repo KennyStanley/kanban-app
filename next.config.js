f689b0c2f593023e72f1f26df71195b87e266c3b
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-02 10:39:07 -0500

    Necessary change for react-beautiful-dnd

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false, 
+  // Setting reactStrictMode: true, breaks react-beautiful-dnd
+  //https://github.com/atlassian/react-beautiful-dnd/issues/1617#issuecomment-574888366
 }
