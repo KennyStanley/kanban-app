01f61640cc254c424a2ca666a427b44b613b5da3
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-03 21:39:07 -0500

    Updated task logic for supabase

--- src/contexts/TasksProvider.tsx
+++ src/contexts/TasksProvider.tsx
@@ -1,11 +1,14 @@
-import { Task } from "@/components/Task/types"
-import { createContext, useContext, useState, useCallback, Dispatch, SetStateAction } from "react";
+import { NewTask, Task } from "@/components/Task/types"
+import { createContext, useContext, useEffect, useState, useCallback, Dispatch, SetStateAction } from "react";
+import { supabase } from '@/utils/supabase'
 
 type TasksProviderOutput = {
     tasks: Task[] | undefined
     setTasks: Dispatch<SetStateAction<Task[]>>
-    getTaskById(id: string): Task | undefined
+    getTaskById(id: number): Task | undefined
+    addTask(newTask: NewTask): void
     updateTask(updatedTask: Task): void
+    deleteTask(id: number): void
 }
 
 const TasksContext = createContext<TasksProviderOutput | undefined>(undefined)
@@ -15,37 +18,62 @@ export function useTasks() {
 }
 
 export function TasksProvider(props: {children?: any}) {
-    const [tasks, setTasks] = useState<Task[]>([
-        {
-            id: '1',
-            title: 'Make Kanban App',
-            position: 'backlog',
-            isDone: true,
-            developer: 'Kenny'
-        },
-        {
-            id: '2',
-            title: 'Make VR Demo app',
-            details: 'Hub with links to all working VR demos',
-            position: 'backlog',
-            isDone: false,
-            developer: 'Kenny'
-        }
-    ])
+    const [tasks, setTasks] = useState<Task[]>([])
 
-    const getTaskById = useCallback((id: string) => {
+    const fetchTasks = useCallback(async () => {
+        let { data: tasks, error } = await supabase.from('tasks').select('*')
+        if (error) console.log('error', error)
+        else setTasks(tasks as Task[])
+        console.log(`Fetched Tasks from supabaseDB`)
+        console.log(tasks)
+    }, [setTasks])
+
+    const getTaskById = useCallback((id: number) => {
         return tasks.find(task => task.id === id)
     }, [tasks])
 
-    const updateTask = useCallback((updatedTask: Task) => {
+    const addTask = async (newTask: NewTask) => {
+        // setTasks((prevTasks: Task[]) => ({...prevTasks, newTask}))
+
+        const { data: task, error } = await supabase.from('tasks').insert([newTask])
+        if (error) console.log(error)
+        else console.log('Added Task', task)
+        fetchTasks()
+    }
+
+    const updateTask = useCallback(async (updatedTask: Task) => {
         setTasks((prevTasks: Task[]) => prevTasks.map(task => {
             if (task.id !== updatedTask.id) return task
             else return updatedTask
         }))
+        
+        const { data: task, error } = await supabase.from('tasks').update(updatedTask).match({ id: updatedTask.id})
+        if (error) console.log('error', error)
+        else console.log('Updated Task', task)
+        
     }, [setTasks])
 
+    const deleteTask = async (id: number) => {
+        const { data: task, error } = await supabase.from('tasks').delete().match({ id })
+        if (error) console.log(error)
+        else console.log(task)
+        fetchTasks()
+    }
+
+    useEffect(() => {
+        fetchTasks()
+    }, [])
+
     return (
-        <TasksContext.Provider value={{tasks, setTasks, getTaskById, updateTask}}>
+        <TasksContext.Provider 
+            value={{
+                tasks, 
+                setTasks, 
+                getTaskById, 
+                addTask, 
+                updateTask, 
+                deleteTask
+            }}>
             {props.children}
         </TasksContext.Provider>
     )
