b95bc298a06bb926f6d60c532e7f692714be0e5d
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-03 21:48:27 -0500

    Changed delete button color to red

--- src/components/Task/TaskComponent.tsx
+++ src/components/Task/TaskComponent.tsx
@@ -138,14 +138,14 @@ const TaskComponent = (props: { task: Task }) => {
                     <div className="flex justify-center gap-2 m-2">
                     <button
                         type="button"
-                        className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="inline-flex items-center px-4 py-2 border border-gray-300 text-base font-medium rounded-md shadow-sm text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         onClick={() => setIsDeleting(false)}
                     >
                         Cancel
                     </button>
                         <button
                             type="button"
-                            className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             onClick={() => {
                                 setIsDeleting(false)
                                 if (deleteTask) deleteTask(props.task.id)
