e68211e4a01090ca9fa5ada0a7936aaf131270af
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-13 10:27:57 -0500

    Necessary change for react-beautiful-dnd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: false, 
   // Setting reactStrictMode: true, breaks react-beautiful-dnd
-  //https://github.com/atlassian/react-beautiful-dnd/issues/1617#issuecomment-574888366
+  // Apparently enabling strict mode causes multiple renders and it just isn't SSR friendly.
+  // https://github.com/atlassian/react-beautiful-dnd/issues/1617#issuecomment-574888366
 }
